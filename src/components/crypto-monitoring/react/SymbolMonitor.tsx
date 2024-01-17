1e889681ee9122d5938615e5f9fa02f60530215a
Cristian Pereyra <cristian.pereyra.dev@gmail.com>
2024-01-17 14:22:45 -0300

    Update

--- src/components/crypto-monitoring/react/SymbolMonitor.tsx
+++ src/components/crypto-monitoring/react/SymbolMonitor.tsx
@@ -7,11 +7,11 @@ export default function SymbolMonitor(props: SymbolMonitorProps) {
 
   return (
     <>
-      <div className="grid grid-cols-2 gap-4 text-slate-950">
-        <div className="rounded-t bg-cyan-400 px-2 py-1">
+      <div className="grid grid-cols-2 gap-4 text-slate-50">
+        <div className="rounded-t bg-gray-800 px-2 py-1">
           <strong>Buy</strong>
         </div>
-        <div className="rounded-t bg-cyan-400 px-2 py-1">
+        <div className="rounded-t bg-gray-800 px-2 py-1">
           <strong>Sell</strong>
         </div>
       </div>
