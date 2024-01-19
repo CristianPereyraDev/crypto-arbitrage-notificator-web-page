b246fc7bb1e6d81f7714ec29d9f42321cf1234f6
Cristian Pereyra <cristian.pereyra.dev@gmail.com>
2024-01-19 13:36:00 -0300

    Update

--- src/components/crypto-monitoring/react/SymbolMonitor.tsx
+++ src/components/crypto-monitoring/react/SymbolMonitor.tsx
@@ -5,20 +5,32 @@ type SymbolMonitorProps = { crypto: string; fiat: string };
 export default function SymbolMonitor(props: SymbolMonitorProps) {
   const symbol: { crypto: string; fiat: string } = props;
 
+  document.addEventListener("htmx:wsAfterMessage", ((event: CustomEvent) => {
+    const targetId = document
+      .createRange()
+      .createContextualFragment(event.detail.message).firstElementChild?.id;
+    if (targetId !== undefined) {
+      //document.getElementById(targetId)?.classList.remove("htmx-added");
+    }
+  }) as EventListener);
+
   return (
     <>
       <div className="grid grid-cols-2 gap-4 text-slate-50">
+        <section class="col-span-full">
+          <h2>Volumen</h2>
+        </section>
         <div className="rounded-t bg-gray-800 px-2 py-1">
-          <strong>Buy</strong>
+          <strong>Compra</strong>
         </div>
         <div className="rounded-t bg-gray-800 px-2 py-1">
-          <strong>Sell</strong>
+          <strong>Venta</strong>
         </div>
       </div>
 
       <div
         id={`${symbol.crypto}-${symbol.fiat}-container`}
-        className="grid grid-cols-2 gap-4"
+        className="symbol-monitor-container grid grid-cols-2 gap-4"
         data-asset={symbol.crypto}
         data-fiat={symbol.fiat}
       />
--- src/components/crypto-monitoring/react/SymbolTabs.tsx
+++ src/components/crypto-monitoring/react/SymbolTabs.tsx
@@ -1,10 +1,7 @@
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Box from "@mui/material/Box";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "./styles/tabs.css";
 
 import type { ICryptoPair } from "~/types";
-import { useState } from "react";
-import CustomTabPanel from "./CustomTabPanel";
 import SymbolMonitor from "./SymbolMonitor";
 
 type SymbolTabsProps = {
@@ -13,36 +10,25 @@ type SymbolTabsProps = {
 
 export default function SymbolTabs(props: SymbolTabsProps) {
   const { pairs } = props;
-  const [value, setValue] = useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
 
   return (
     <div className="px-4">
-      <div className="sticky top-0 rounded-b-md bg-zinc-800">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
+      <Tabs forceRenderTabPanel={true}>
+        <TabList className={["react-tabs__tab-list"]}>
           {pairs.map((pair, index) => (
             <Tab
               key={index}
-              label={`${pair.crypto}-${pair.fiat}`}
-              id={`simple-tab-${index}`}
-              aria-controls={`simple-tabpanel-${index}`}
-              className="text-amber-300"
-            />
+              className={["react-tabs__tab"]}
+            >{`${pair.crypto}-${pair.fiat}`}</Tab>
           ))}
-        </Tabs>
-      </div>
-      {pairs.map((pair, index) => (
-        <CustomTabPanel key={index} index={index} value={value}>
-          <SymbolMonitor crypto={pair.crypto} fiat={pair.fiat} />
-        </CustomTabPanel>
-      ))}
+        </TabList>
+
+        {pairs.map((pair, index) => (
+          <TabPanel key={index}>
+            <SymbolMonitor crypto={pair.crypto} fiat={pair.fiat} />
+          </TabPanel>
+        ))}
+      </Tabs>
     </div>
   );
 }
